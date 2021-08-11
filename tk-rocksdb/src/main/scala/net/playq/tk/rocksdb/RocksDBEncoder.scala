af1d034ba66bb1f6025009be1dc5a874e7e71d8e
YaroslavSahach <ysahach@playq.net>
2021-08-11 17:42:37 +0300

    added few rocksdb encoders

--- tk-rocksdb/src/main/scala/net/playq/tk/rocksdb/RocksDBEncoder.scala
+++ tk-rocksdb/src/main/scala/net/playq/tk/rocksdb/RocksDBEncoder.scala
@@ -1,5 +1,7 @@
 package net.playq.tk.rocksdb
 
+import java.util.UUID
+
 trait RocksDBEncoder[T] {
   def toBytes(t: T): Array[Byte]
 }
@@ -9,5 +11,10 @@ object RocksDBEncoder {
 
   def toBytes[T: RocksDBEncoder](t: T): Array[Byte] = RocksDBEncoder[T].toBytes(t)
 
-  implicit val stringKeyEncoder: RocksDBEncoder[String] = _.getBytes
+  implicit val stringRocksDBEncoder: RocksDBEncoder[String] = _.getBytes
+  implicit val byteRocksDBEncoder: RocksDBEncoder[Byte]     = BigInt(_).toByteArray
+  implicit val shortRocksDBEncoder: RocksDBEncoder[Short]   = BigInt(_).toByteArray
+  implicit val intRocksDBEncoder: RocksDBEncoder[Int]       = BigInt(_).toByteArray
+  implicit val longRocksDBEncoder: RocksDBEncoder[Long]     = BigInt(_).toByteArray
+  implicit val uuidRocksDBEncoder: RocksDBEncoder[UUID]     = _.toString.getBytes
 }
