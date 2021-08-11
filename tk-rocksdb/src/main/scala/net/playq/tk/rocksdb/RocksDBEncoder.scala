1fad0659c30299bbe3a2fe2f6c2d5a273bece306
YaroslavSahach <ysahach@playq.net>
2021-08-11 17:56:28 +0300

    fix warnings

--- tk-rocksdb/src/main/scala/net/playq/tk/rocksdb/RocksDBEncoder.scala
+++ tk-rocksdb/src/main/scala/net/playq/tk/rocksdb/RocksDBEncoder.scala
@@ -9,11 +9,10 @@ trait RocksDBEncoder[T] {
 object RocksDBEncoder {
   @inline def apply[T](implicit ev: RocksDBEncoder[T]): ev.type = ev
 
-  def toBytes[T: RocksDBEncoder](t: T): Array[Byte] = RocksDBEncoder[T].toBytes(t)
-
+  def toBytes[T: RocksDBEncoder](t: T): Array[Byte]         = RocksDBEncoder[T].toBytes(t)
   implicit val stringRocksDBEncoder: RocksDBEncoder[String] = _.getBytes
-  implicit val byteRocksDBEncoder: RocksDBEncoder[Byte]     = BigInt(_).toByteArray
-  implicit val shortRocksDBEncoder: RocksDBEncoder[Short]   = BigInt(_).toByteArray
+  implicit val byteRocksDBEncoder: RocksDBEncoder[Byte]     = b => BigInt(b.toInt).toByteArray
+  implicit val shortRocksDBEncoder: RocksDBEncoder[Short]   = s => BigInt(s.toInt).toByteArray
   implicit val intRocksDBEncoder: RocksDBEncoder[Int]       = BigInt(_).toByteArray
   implicit val longRocksDBEncoder: RocksDBEncoder[Long]     = BigInt(_).toByteArray
   implicit val uuidRocksDBEncoder: RocksDBEncoder[UUID]     = _.toString.getBytes
