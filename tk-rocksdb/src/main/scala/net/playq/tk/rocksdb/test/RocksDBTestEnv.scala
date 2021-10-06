d29d1e09d30055ddbb8e6e30d3c661a33a0ec577
YaroslavSahach <ysahach@playq.net>
2021-10-06 19:59:14 +0300

    fixed RocksDBTestEnv

--- tk-rocksdb/src/main/scala/net/playq/tk/rocksdb/test/RocksDBTestEnv.scala
+++ tk-rocksdb/src/main/scala/net/playq/tk/rocksdb/test/RocksDBTestEnv.scala
@@ -1,24 +1,18 @@
 package net.playq.tk.rocksdb.test
 
-import distage.{DIKey, ModuleDef, Scene, TagKK}
+import distage.{DIKey, ModuleDef, TagKK}
 import izumi.distage.model.definition.Lifecycle
 import izumi.distage.testkit.TestConfig
 import izumi.functional.bio.{F, IO2}
+import net.playq.tk.rocksdb.RocksBaseId
 import net.playq.tk.rocksdb.test.RocksDBTestEnv.RocksDBCleanup
-import net.playq.tk.rocksdb.{RocksBaseId, RocksDBConfig}
-import net.playq.tk.test.{ForcedRoots, MemoizationRoots, ModuleOverrides, WithProduction}
+import net.playq.tk.test.{ForcedRoots, ModuleOverrides, WithProduction}
 
 import java.io.File
 import scala.reflect.io.Directory
 
-trait RocksDBTestEnv[F[+_, +_]] extends MemoizationRoots with ModuleOverrides with ForcedRoots with WithProduction {
+trait RocksDBTestEnv[F[+_, +_]] extends ModuleOverrides with ForcedRoots with WithProduction {
   implicit val tagBIO: TagKK[F]
-
-  abstract override def memoizationRoots: TestConfig.PriorAxisDIKeys = {
-    super.memoizationRoots ++
-      Map(0 -> Map(Scene.Managed -> DIKey[RocksDBConfig])) +
-      (1 -> DIKey[RocksDBCleanup[F]])
-  }
 
   abstract override def moduleOverrides: distage.Module = super.moduleOverrides ++ new ModuleDef {
     make[RocksDBCleanup[F]].fromResource[RocksDBCleanup[F]]
